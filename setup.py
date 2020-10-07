c3d454b5447b463785b4769c84065c7bb82134e4
Jon Ezeiza <jon@scitodate.com>
2020-10-07 11:38:06 +0000

    Added -Wno-error flags for GCC 9

--- setup.py
+++ setup.py
@@ -43,7 +43,8 @@ class CMakeBuild(build_ext):
         extdir = os.path.abspath(
             os.path.dirname(self.get_ext_fullpath(ext.name)))
         cmake_args = ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir,
-                      '-DPYTHON_EXECUTABLE=' + sys.executable]
+                      '-DPYTHON_EXECUTABLE=' + sys.executable,
+                      '-DCMAKE_CXX_FLAGS=-Wno-error=deprecated-copy -Wno-error=pessimizing-move -Wno-error=sign-compare']
 
         cfg = 'Debug' if self.debug else 'Release'
         build_args = ['--config', cfg]
